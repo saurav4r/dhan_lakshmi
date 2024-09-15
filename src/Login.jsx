0369bca661a92a3f59eebc1af3291e84226f6308
GAUARVCHIT <gaurav198.rr@gmail.com>
2024-09-16 02:24:11 +0530

    url changes for vercel

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,66 +1,90 @@
-import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import './login.css';
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./login.css"
 
 export const Login = () => {
-  const navigate = useNavigate();
-  const [email1, setEmail1] = useState('');
-  const [pass1, setPass1] = useState('');
-  const [message,setMessage]=useState('');
+  const navigate = useNavigate()
+  const [email1, setEmail1] = useState("")
+  const [pass1, setPass1] = useState("")
+  const [message, setMessage] = useState("")
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
   }
-  
+
   async function CHEACK() {
     try {
-      const response = await fetch("http://localhost:3003/login", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          password: pass1,
-          email: email1
-        })
-      });
-  
-      const data = await response.json();
-      console.log(data);
-      localStorage.setItem("token",data.token);
-      console.log(data.token);
-  
+      const response = await fetch(
+        "https://backend-of-dhan-lakshmi-f7rga455d-sauravs-projects-e1caf8bf.vercel.app/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            password: pass1,
+            email: email1,
+          }),
+        }
+      )
+
+      const data = await response.json()
+      console.log(data)
+      localStorage.setItem("token", data.token)
+      console.log(data.token)
+
       if (data.message === "login-successful") {
-        navigate("/form");
+        navigate("/form")
       } else {
-        setMessage(data.message);
+        setMessage(data.message)
       }
     } catch (error) {
-      console.error(error);
-      setMessage("An error occurred during login.");
+      console.error(error)
+      setMessage("An error occurred during login.")
     }
   }
-  
 
   return (
     <div className="login-back">
       <div className="auth-form-container">
-      <h2>Login</h2>
+        <h2>Login</h2>
 
-      
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input type="email" placeholder="youremail@gmail.com" id="email" onChange={(e) => { setEmail1(e.target.value) }} />
-        <label htmlFor="password" color="white">Password</label>
-        <input type="password" placeholder="enter your password" id="password"  onChange={(e) => { setPass1(e.target.value) }} />
-        <button type="submit" onClick={CHEACK}>login</button>
-        <h3>{message}</h3>
-      </form>
-      
-      <button className="link-btn" onClick={() => {
-        navigate("/Register")
-      }}>don't have an account? Register here.</button>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="youremail@gmail.com"
+            id="email"
+            onChange={(e) => {
+              setEmail1(e.target.value)
+            }}
+          />
+          <label htmlFor="password" color="white">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="enter your password"
+            id="password"
+            onChange={(e) => {
+              setPass1(e.target.value)
+            }}
+          />
+          <button type="submit" onClick={CHEACK}>
+            login
+          </button>
+          <h3>{message}</h3>
+        </form>
+
+        <button
+          className="link-btn"
+          onClick={() => {
+            navigate("/Register")
+          }}
+        >
+          don't have an account? Register here.
+        </button>
+      </div>
     </div>
-    </div>
-  );
+  )
 }
